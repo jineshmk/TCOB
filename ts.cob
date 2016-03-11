3b589302596f370f4f3be9f7c7c3cdf5582ab60d
Jinesh M.K <mkjinesh@gmail.com>
2016-03-11 16:23:31 +0530

    Remove the external capacity calculation

--- ts.cob
+++ ts.cob
@@ -17,43 +17,29 @@ class road extends time{
         int [10][]V;
         %T-traffic
         int []T;
+
     constraints
-        %Capacity constraints
+       
+        forall J in 1..Time:
+            forall I in 1..4: (V[J,I]>=0;V[J,I]<=1);
+        %Traffic must be less than capacity
         forall I in 1..Time:
-            (T[I]<=C);
+            (T[I]>=0;T[I]<=C);
+        %Count the number of vehicle present in the road
+        forall J in 1..Time: 
+             sumlist(V[J],T[J]);
+        
+   predicates
+        sumlist([], 0).
+        sumlist([H|T], Sum) :-
+            sumlist(T, Rest),{Sum =H + Rest},'!'.
+       
     constructors road(ID1,S1,E1,D1,C1){
             ID=ID1; S=S1; E=E1; D=D1; C=C1;
     }
-        
 }
 
-class trafficflow extends time{
-    attributes 
-        road []R;
-        vehicle[] V;
-    constraints
-        %Count number of vehicle on each road
-        forall Ro in R: 
-            forall J in 1..Time: 
-                (countvehicle(Ro.V[J],Ro.T[J]));
-       
-       %Basic safety property,two vehicle does not exist at 
-        %the same road at same position.
-        forall V1 in V:
-            forall V2 in V:
-                forall J in 1..Time: 
-                    (not (V1.P[J]=V2.P[J]) :- 
-                        not (V1.ID = V2.ID),V1.RT[J]=V2.RT[J]);
-    predicates
-        isset(1,1):-'!'.
-        isset(_,0).
-        countvehicle([],0):- '!'.
-        countvehicle([H|T],C):- isset(H,N),countvehicle(T,M),{C=N+M},'!'.
-    constructors trafficflow(R1,V1){
-        R=R1;
-        V=V1;
-    }
-}
+
 
 
 %Class vehicle type- represents the basic properties
@@ -74,20 +60,20 @@ class vehicle extends time{
         string S,E;
         real [10] V,A,P;
         vtype VT;
-        %R- roads exists in the system
+        %R- roads 
         road []R;
         real [5][4]RI;
         %Road trace
         int [10]RT;
+        int []Path;
         %Total number of roads
         int NR;
     constraints
         %Extract ID,S,E,D from every road class, used for path calculation
         forall J in 1..NR:  
-                RI[J]=[R[J].ID,R[J].S,R[J].E,R[J].D];
+                    RI[J]=[R[J].ID,R[J].S,R[J].E,R[J].D];
         %Find the path
         spath(S,E,RI,Path);
-        dump(Path);
         forall J in 1..Time:  (A[J] <= VT.MaxA;
 			     A[J] >=-VT.MaxB;
 			     V[J]>0;
@@ -96,38 +82,37 @@ class vehicle extends time{
 			    %position and Velocity constraints
 			    P[J] = V[J-1]+A[J]/2; 
 			    V[J]=V[J-1]+A[J]);
-        %Identify the current road and mark the existence in the road
-        forall J in 1..Time :(currentroad(RI,Path,P[J],RT[J]);
-                                R[RT[J]].V[J,ID]=1);
-                                
+        %IDentify the current road and mark 1 in the corresponding road
+        forall J in 1..Time:(getroad(Path,P[J],RT[J]);
+                    R[RT[J]].V[J,ID]=1);
+                            
     predicates
         
-        %Return the series of Road-ID, which represents the shortestpath 
+        %Return the series of Road-ID and distance, 
+        %which represents the shortestpath 
         spath(X,Y,R,P) :- 
-            findall(T,path(X,Y,R,T),Temp),mindist(Temp,[P|_]).
-        %Get the minimum distance path
+            findall(T,path(X,Y,R,T),Temp),mindist(Temp,[P|_]),'!'.
+            
+        %Get path from S to E with distance
+        path(X,Y,R,P):- directroad(X,Y,R,P).
+        path(X,Y,R,[[HID,HD|TID],Di]):- 
+                directroad(X,Z,R,[[HID,HD],HD]),
+                    path(Z,Y,R,[TID,TD]),{Di=HD+TD}.
+        directroad(X,Y,[[ID,X,Y,D]|_],[[ID,D],D]).
+        directroad(X,Y,[_|T],P):- directroad(X,Y,T,P).
+
+         %Get the minimum distance path
         mindist([X], X) :- '!'.
         mindist([[P1,X],[P2,Y]|Tail], N):- 
             X > Y,mindist([[P2,Y]|Tail], N).
         mindist([[P1,X],[P2,Y]|Tail], N):- 
             Y >= X,mindist([[P1,X]|Tail], N).
-            
-        %Get path from S to E with distance
-        path(X,Y,R,P):- directroad(X,Y,R,P).
-        path(X,Y,R,[[HID|TID],Di]):- 
-                directroad(X,Z,R,[[HID],HD]),
-                    path(Z,Y,R,[TID,TD]),{Di=HD+TD}.
-        directroad(X,Y,[[ID,X,Y,D]|_],[[ID],D]).
-        directroad(X,Y,[_|T],P):- directroad(X,Y,T,P).
+        
+        getroad([ID,Di],D,ID)  :- {Di<D},'!'.
+        getroad([ID,Di|_],D,ID):- {Di>=D},'!'.
+        getroad([_,Di|T],D,ID) :- {DDi = D - Di},getroad(T,DDi,ID),'!'.
     
-        %Get the current road based on displacement and path
-        currentroad(_,[ID],D,ID):-'!'.
-        currentroad([[ID,_,_,Di]|_],[ID|_],D,ID) :- {Di>D}, '!'.
-        currentroad([[ID,_,_,Di]|T],[ID|RR],D,R) :- 
-                            T = [_|_],{DDi = D - Di}, 
-                            currentroad(T,RR,DDi,R).
-        currentroad([[_,_,_,_]|T],ID,D,R) :-   
-                            currentroad(T,ID,D,R).  
+        
                             
         
     constructors vehicle(ID1,S1,E1,V1,A1,P1,R1,NR1,VT1){
@@ -144,6 +129,26 @@ class vehicle extends time{
     }   
 }
 
+class trafficflow extends time{
+    attributes 
+        road []R;
+        vehicle[] V;
+    constraints
+          
+        %Basic safety property,two vehicle does not exist at 
+        %the same road at same position.
+        forall V1 in V:
+            forall V2 in V:
+                forall J in 1..Time: 
+                    (not (V1.P[J]=V2.P[J]) :- 
+                        not (V1.ID = V2.ID),V1.RT[J]=V2.RT[J]);
+    
+    constructors trafficflow(R1,V1){
+        R=R1;
+        V=V1;
+    }
+}
+
 class environment{
 
     attributes  
@@ -156,7 +161,6 @@ class environment{
         %----Printing-------
         forall I in V:(write('-----------------------------');
             write('\nP--');dump(I.P);write('V--');dump(I.V);write('A--');dump(I.A);write('R--');dump(I.RT));
-        
         
     predicates
          dump([]):-write('\n').
